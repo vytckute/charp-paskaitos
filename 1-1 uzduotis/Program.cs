fa06c0dbfb9a9490022f737f54c14d1e4b9a3eb1
Vita <Vita@VITA-PC>
2017-11-14 19:36:21 +0200

    Add project files.

---
+++ 1-1 uzduotis/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_1_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vardas = "Vita";
+            var pavarde = "Luko";
+            var amzius = 30;
+            var ugis = 1.50f;
+            var svoris = 60.5; //kg
+            var aukstoji_mokykla = "KTU";
+            var akademines_grupes_kodas = "1A";
+            var kursas = 1;
+            var studiju_programu_pavadinimas = "Ccc";
+            var atsiskaitytu_kreditu_skaicius = 150;
+
+
+
+        }
+    }
+}
