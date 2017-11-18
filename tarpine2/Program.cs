cb8b06c3334fba06e16a9ded574330750d447499
Vita <Vita@VITA-PC>
2017-11-18 14:30:41 +0200

    11-18

---
+++ tarpine2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarpine2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var suma = 0;
+            var kvsuma = 0;
+            for (int i = 1; i < 101; i++)
+            {
+                suma += i;
+                kvsuma += (i*i);
+
+            }
+            Console.WriteLine(suma*suma-kvsuma);
+        }
+    }
+}
